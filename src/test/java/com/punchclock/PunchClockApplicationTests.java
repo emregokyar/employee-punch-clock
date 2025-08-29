339b4a31f0794099fe62ac906419aa792c5e6327
emregokyar <emregokyar1940@gmail.com>
2025-08-29 17:40:48 +0300

    Initial Commit

---
+++ src/test/java/com/punchclock/PunchClockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punchclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
